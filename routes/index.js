974c6a7949c296d8877217fce2cb17fdb28c0e59
unknown <j.camilo.g.perez@gmail.com>
2024-10-01 19:50:27 -0500

    conexion front

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ const express=require('express')
 const enrutador=express.Router()
 const resp=require('../src/util/rsp')
 
-enrutador.get('/login', (req,res)=>{
-    if(req.body.user=='juankgalvan'){
+enrutador.post('/login', (req,res)=>{
+    if(req.body.user=='j.camilo.g.perez@gmail.com'& req.body.pass=='12345'){
         resp.success(req,res,200,'Exitoso')
     }
     resp.success(req,res,500,'Error Interno')
 })
 
-enrutador.post('/create', (req,res)=>{
+/*enrutador.post('/create', (req,res)=>{
     resp.success(req,res,200,'Usuario Creado')
-})
+})*/
 
 module.exports=enrutador
